3383fc79a33a331aade5c504800b487d918f54d0
NoranPeLage <ronan.lepage94@gmail.com>
2020-11-12 10:37:35 +0100

    init jest and basic test for App component

---
+++ App.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import renderer, { ReactTestRendererJSON } from 'react-test-renderer';
+import App from './App';
+
+describe('<App />', () => {
+    it('has 1 child', () => {
+        const tree = renderer.create(<App />).toJSON() as ReactTestRendererJSON;
+        expect(tree?.children?.length).toBe(1);
+    });
+});
