fb3900ddb3493030f8ad3a3e0f6f5ad7bd88483a
NoranPeLage <ronan.lepage94@gmail.com>
2020-11-14 20:36:11 +0100

    Add ESLint & Prettier

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  parser: "@typescript-eslint/parser", // Specifies the ESLint parser
+  plugins: ["@typescript-eslint", "react-native", "prettier"],
+  parserOptions: {
+    sourceType: "module"
+  },
+  extends: [
+    "airbnb",
+    "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint", // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+    "plugin:prettier/recommended", // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors.
+    "prettier/react",
+    "prettier/@typescript-eslint"
+  ],
+  settings: {
+    "import/resolver": {
+      node: {
+        extensions: [".ts", ".tsx"]
+      }
+    }
+  },
+  rules: {
+    "no-use-before-define": "off",
+    "no-unused-vars": 2,
+    "@typescript-eslint/no-unused-vars": 2,
+    "@typescript-eslint/no-use-before-define": ["error", { variables: false }],
+    "@typescript-eslint/explicit-member-accessibility": "off",
+    "react/jsx-filename-extension": ["error", { extensions: [".tsx"] }],
+    "react-native/no-unused-styles": 2,
+    "react-native/split-platform-components": 2,
+    "react-native/no-inline-styles": 0,
+    "react-native/no-color-literals": 2,
+    "react-native/no-raw-text": 0,
+    "prettier/prettier": 2,
+    "import/extensions": "off",
+  }
+};
--- App.tsx
+++ App.tsx
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+// eslint-disable-next-line import/no-unresolved
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+const white = '#fff';
+
+export default function App(): JSX.Element {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar />
     </View>
   );
 }
@@ -14,8 +17,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000',
+    backgroundColor: white,
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: 'center'
+  }
 });
